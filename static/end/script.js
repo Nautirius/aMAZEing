732c751bb6e56cbf877866b020a43b9e0249fdd5
FreestyleXV <mrkuba2003.Ja@gmail.com>
2021-06-16 20:23:32 +0200

    baba

--- static/end/script.js
+++ static/end/script.js
@@ -10,9 +10,9 @@ fetch('http://localhost:3000/endPrintData', {
                 console.log(res.room)
 
                 if(res.room.win){
-
+                    app.innerHTML=`<h1 style="color:green;">Gratulacje, wygrałeś!</h1><h2>Twój czas to: ${Math.floor(res.room.time/1000)}s</h2><img src="Science_Emoji_Vita.jpg" alt="Wygrana_zdj">`
                 }else{
-
+                    app.innerHTML=`<h1 style="color:red;">Przegrana</h1><h2>Czas po jakim się poddałeś: ${Math.floor(res.room.time/1000)}s</h2><img id="img" src="./20210529_235718.jpg" alt="Przegrana_zdj">`
                 }
             }
         })
